366a7ab108e779a7fed994a3b7d87fda5665d5b9
Chonthicha Jessie Pai-Ngam <chonthicha.pai-ngam@edu.sait.ca>
2024-01-09 16:44:47 -0700

    update page layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
+'use client'
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <>
-      <p>Hello world</p>
-      <p>Welcome to CPRG306</p>
-    </>
+    <Wrapping>
+      <Content>Hello world</Content>
+      <Content>Welcome to CPRG306</Content>
+    </Wrapping>
   );
 };
 export default Home;
+
+export const Content = styled.p`
+  font-size: 80px;
+`
+export const Wrapping = styled.div`
+  width: 100%;
+  height: 1800px;
+  background: pink;
+`
