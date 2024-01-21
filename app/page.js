3d54666288299b8ac4d4cdf28050ac18a5069bbe
Chonthicha Jessie Pai-Ngam <chonthicha.pai-ngam@edu.sait.ca>
2024-01-20 20:07:57 -0700

    update layout bg

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ const Home = () => {
       <h1 class ="text-xl font-bold mt-5 ml-5 text-white" >CPRG 306: Web Development 2 - Assignments</h1>
       <div class ="md:flex flex-col text-white mt-5 ml-10">
         <Link href="./week-2">Week 2 Assignment</Link>
-        <Link href="">Week 3 Assignment</Link>
+        <Link href="./week-3">Week 3 Assignment</Link>
         <Link href="">Week 4 Assignment</Link>
         <Link href="">Week 5 Assignment</Link>
         <Link href="">Week 6 Assignment</Link>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,10 +2,10 @@ import ItemList from "./item-list";
 
 const Page = () => {
   return (
-    <main class="bg-slate-950 h-screen bg-cover">
-      <h1 class="text-3xl font-bold m-2 text-white">Shopping List</h1>
-      <ItemList/>
-    </main>
+      <main class="bg-slate-950 md:flex flex-col ">
+        <h1 class="text-3xl font-bold m-2 text-white">Shopping List</h1>
+        <ItemList />
+      </main>
   );
 };
 export default Page;
