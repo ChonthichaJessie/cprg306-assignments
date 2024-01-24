2017659c55e4b0e8ce9c50a0fe9fc169d0886d70
Chonthicha Jessie Pai-Ngam <chonthicha.pai-ngam@edu.sait.ca>
2024-01-24 12:21:18 -0700

    update week3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 const Item = (props) => {
   const { name, quantity, category } = props;
   return (
-    <div class="bg-slate-900 bg-cover max-w-sm p-2 m-4 ">
+    <div class="bg-slate-900 bg-cover max-w-sm p-2 m-4">
       <div class="text-white font-bold text-xl" >{name}</div>
       <div class="text-white text-l">
         buy {quantity} in {category}
