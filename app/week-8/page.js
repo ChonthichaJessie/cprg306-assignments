cdd56e840cfbb0d0e1b66ca9a9b87806d2e74238
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-03-16 11:20:45 -0600

    update week 8

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -2,24 +2,42 @@
 //Need a parent to be provider (higher one level) => chose layer.js
 "use client";
 
+import Link from "next/link";
 import { useUserAuth } from "./_utils/auth-context";
 
 const Page = () => {
   // Use the useUserAuth hook to get the user object and the login and logout functions
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const isSignIn = () => {
+    return (
+      <div>
+        <button onClick={firebaseSignOut}>Sign Out</button>
+        <br />
+        <Link href="./week-7" className="bg-red">
+          Continue to your Shopping List
+        </Link>
+      </div>
+    );
+  };
 
-  return(
-    <div>
-        <h1>Week 8</h1>
+  return (
+    <main>
+      <div className="m-2">
+        <h1 className="text-3xl font-bold  text-white">
+          Week 8 Shopping List App
+        </h1>
         <p>{user ? "Hi! there!" : "Please sign in"}</p>
         {user && user.displayName}
         {user && user.email}
         <p>
-            {user?(<button onClick={firebaseSignOut}>Sign Out</button>):(<button onClick={gitHubSignIn}>Sign in Github</button>)}
+          {user ? (
+            isSignIn()
+          ) : (
+            <button onClick={gitHubSignIn}>Sign in Github</button>
+          )}
         </p>
-
-    </div>
-  )
-
+      </div>
+    </main>
+  );
 };
 export default Page;
